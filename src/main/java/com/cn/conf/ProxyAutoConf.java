4d518eef21e43c73439f42a12494c37326fe7538
luoyx <luoyx@bsfit.com.cn>
2023-02-09 10:05:09 +0800

    feat：SpringBoot 反向代理

--- src/main/java/com/cn/conf/ProxyAutoConf.java
+++ src/main/java/com/cn/conf/ProxyAutoConf.java
@@ -1,41 +1,28 @@
 package com.cn.conf;
 
-import com.cn.model.ProxyProperties;
-import com.cn.utils.SpringContextUtil;
-import org.apache.commons.collections4.CollectionUtils;
 import org.mitre.dsmiley.httpproxy.ProxyServlet;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.servlet.ServletContainerInitializer;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-import java.util.Set;
-
 @Configuration
-@EnableConfigurationProperties(ProxyProperties.class)
-@ConditionalOnProperty(value = ProxyProperties.CONDITIONAL, havingValue = "true")
-public class ProxyAutoConf implements ServletContainerInitializer {
+public class ProxyAutoConf  {
+    @Value("${proxy.showLog}")
+    private String showLog;
 
-    public ProxyProperties getProxyProperties() {
-        return SpringContextUtil.getBean(ProxyProperties.class);
-    }
+    @Value("${proxy.servletUrl}")
+    private String servletUrl;
 
-    @Override
-    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
-        if (!Boolean.TRUE.equals(getProxyProperties().getEnabled()) || CollectionUtils.isEmpty(getProxyProperties().getProxyAttributes())){
-            return;
-        }
+    @Value("${proxy.targetUrl}")
+    private String targetUrl;
 
-        for (int i = 0; i < getProxyProperties().getProxyAttributes().size(); i++) {
-            ProxyProperties.ProxyAttribute proxyAttribute = getProxyProperties().getProxyAttributes().get(i);
-            ServletRegistration initServlet = servletContext.addServlet(proxyAttribute.getServletUrl(), ProxyServlet.class);
-            initServlet.addMapping(proxyAttribute.getServletUrl());
-            initServlet.setInitParameter(ProxyServlet.P_TARGET_URI, proxyAttribute.getTargetUrl());
-            initServlet.setInitParameter(ProxyServlet.P_LOG, getProxyProperties().getShowLog().toString());
-            initServlet.setInitParameter(ProxyServlet.P_PRESERVECOOKIES, Boolean.TRUE.toString());
-        }
+    @Bean
+    public ServletRegistrationBean proxyServletRegistration(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new ProxyServlet(), servletUrl);
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_TARGET_URI, targetUrl);
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_LOG, showLog);
+
+        return servletRegistrationBean;
     }
 }
